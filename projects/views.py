0d25f074a17f5048ba9709a11ec5c7f8d1d71486
johnboland91 <johnboland91@gmail.com>
2019-07-09 23:07:56 -0500

    fixed views for project index and detail

--- projects/views.py
+++ projects/views.py
@@ -6,11 +6,11 @@ def project_index(request):
     context = {
         'projects': projects
     }
-    return render(request, /project_index.html, context)
+    return render(request, 'project_index.html', context)
 
 def project_detail(request, pk):
     projects = Project.objects.get(pk=pk)
     context = {
         'project': project
     }
-    return render(request, /project_detail.html, context)
+    return render(request, 'project_detail.html', context)
