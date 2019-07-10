c3c0489c4bc2ed31a74badafe6fa649e6bdff8cf
johnboland91 <johnboland91@gmail.com>
2019-07-10 08:15:58 -0500

    dj_database_url

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
